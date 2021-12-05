bb364fc5169faf347bf2770210d297a983129731
Edwin <edwinarandia84@gmail.com>
2021-12-04 22:41:50 -0400

    fix authentication.php

--- php/form/authentication.php
+++ php/form/authentication.php
@@ -3,21 +3,21 @@
     $username = $_POST['username'];  
     $password = $_POST['password'];  
       
-        //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($conn, $username);  
-        $password = mysqli_real_escape_string($conn, $password);  
-      
-        $sql = "select *from login where username = '$username' and password = '$password'";  
-        $result = mysqli_query($conn, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-          
-        if($count == 1){  
-            echo "<h1><center> Login satisfactorio </center></h1>";  
-        }  
-        else{  
-            echo "<h1> Acceso fallido. Nombre de usuario o contraseña no valido</h1>";  
-        }     
-?>  
+    //to prevent from mysqli injection  
+    $username = stripcslashes($username);  
+    $password = stripcslashes($password);  
+    $username = mysqli_real_escape_string($conn, $username);  
+    $password = mysqli_real_escape_string($conn, $password);  
+    
+    $sql = "select *from login where username = '$username' and password = '$password'";  
+    $result = mysqli_query($conn, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result);  
+        
+    if($count == 1){  
+        echo "<h1><center> Login satisfactorio </center></h1>";  
+    }  
+    else{  
+        echo "<h1> Acceso fallido. Nombre de usuario o contraseña no valido</h1>";  
+    }
+?>
