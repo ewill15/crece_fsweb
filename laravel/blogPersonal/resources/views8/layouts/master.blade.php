10f345f4de64ab0f9d9c533f19c15dfbab8f62ac
Edwin <edwinarandia84@gmail.com>
2022-02-27 19:49:33 -0400

    agregada rutas

--- laravel/blogPersonal/resources/views8/layouts/master.blade.php
+++ laravel/blogPersonal/resources/views8/layouts/master.blade.php
@@ -4,17 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Blog Personal</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- Fonts Arizonia-->
-        <link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet"> 
-        <!-- Archivo CSS personalizado -->
-        <link rel="stylesheet" href="../resources/css/styles.css">
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
         <!-- Styles -->
         <style>
@@ -28,7 +21,6 @@
         </style>
     </head>
     <body class="antialiased">
-        @include('partials.header')
         @yield('content')
     </body>
 </html>
