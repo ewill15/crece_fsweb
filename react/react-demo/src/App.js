11f08a139a70a36b8362700baf0ea0fb9beebe1e
Edwin <edwinarandia84@gmail.com>
2022-05-15 16:15:09 -0400

    added react-demo corrections to display components in home

---
+++ react/react-demo/src/App.js
@@ -0,0 +1,32 @@
+/*El logo por defecto importado lo pueden eliminar porque no lo usaremos */
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+
+/*https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom*/
+/*En versiones anteriores*/
+/*import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; */
+/*Actualmente es */
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+function App() {
+  return (
+    <div className="App">
+      <Router> 
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
